75cccecd3510843550224e71b94b0c381d0db5bf
Samuel ODaniels <samodan195@gmail.com>
2022-04-26 16:44:24 +0100

    Pass all debugging tests

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+#require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -10,12 +12,13 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
   smallest_value -= 1
 end
+
 
 # use the stack trace to debug the following method
 # Don't look at this method prior to running the test
@@ -24,7 +27,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -32,15 +35,20 @@ def increment_greatest_value(nested_array)
   greatest_value += 1
 end
 
+
+
+
 # This next exercise might look familiar
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("")
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
+
+# p isogram?("Odin")
 
 # Can you guess what's next?
 # That's right! The final exercise from the lesson, which we'll debug with pry-byebug
@@ -51,6 +59,9 @@ end
 
 def yell_greeting(string)
   name = string
+  #binding.pry
   name = name.downcase
-  greeting = "WASSAP, #{name}!"
+  #binding.pry
+  greeting = "WASSAP, #{name.upcase}!"
+  #binding.pry
 end
